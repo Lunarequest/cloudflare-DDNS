b7e5a97c153c8925797cafa609cdca4e13eb1e79
advaithm <advaith.madhukar@gmail.com>
2020-08-21 11:24:37 +0530

    sys.exit status

--- Update.py
+++ Update.py
@@ -10,11 +10,16 @@ with open("settings.yml", "r") as f:
     email = creds["email"]
     api_key = creds["api_key"]
     domain = creds["domain"]
+    global cf
     cf = CloudFlare.CloudFlare(email=email, token=api_key)
+
 zone = None
 ip = None
 dns = []
 zones = cf.zones.get()
+if len(zones) < 1:
+    print("no zone in account")
+    sys.exit(1)
 
 
 def get_zone_ip(zone, domain):
@@ -28,18 +33,23 @@ def get_zone_ip(zone, domain):
     if found:
         return zone_id
     else:
-        sys.exit("1")
+        print("zone not found")
+        sys.exit(1)
 
 
-def check(cf, zone_id):
+def check(zone_id):
     dns_records = cf.zones.dns_records.get(zone_id)
     current_ip = get("http://ip.42.pl/raw").text
     if current_ip == dns_records["conntent"]:
         print("ip is up to date")
     else:
-        dns_records = cf.zones.dns_records.put(current_ip)
+        dns_records = cf.zones.dns_records.edit(zone_id, current_ip)
 
 
+message = """
+THIS PROGRAM DOES NOT COME WITH ANY WARRENTY
+"""
+print(message)
 zone_id = get_zone_ip(zones, domain)
 check(cf, zone_id)
 
