d8197411f120144f7d56c5c356f3a20ce52cc35f
advaithm <advaith.madhukar@gmail.com>
2020-09-21 20:58:52 +0530

    enabling bandit

--- cloudflare_ddns/gen_settings.py
+++ cloudflare_ddns/gen_settings.py
@@ -3,16 +3,16 @@ def gen_settings():
     """function to genrate the settings.yaml
     :return: None
     """
-    api_key = input("input api key: ").strip().replace(" ","")
-    domain = input("input target domain: ").strip().replace(" ","")
-    zone_id = input("input zone id: ").strip().replace(" ","")
-    check = int(input("input number of subdomains: "))
+    api_key = input("input api key: ").strip().replace(" ","") # nosec
+    domain = input("input target domain: ").strip().replace(" ","") # nosec
+    zone_id = input("input zone id: ").strip().replace(" ","") # nosec
+    check = int(input("input number of subdomains: ")) # nosec
     if check == 0:
         creds = {"api_key": api_key, "domain": domain, "zone_id": zone_id}
     else:
         subdomains = []
         for i in range(0, check):
-            x = input("input subdomain: ").strip().replace(" ","")
+            x = input("input subdomain: ").strip().replace(" ","") # nosec
             subdomains.append(x)
         creds = {
             "api_key": api_key,
