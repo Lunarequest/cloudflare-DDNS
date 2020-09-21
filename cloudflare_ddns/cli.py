c43d3cae86b379e2da2de324666b7ac30f1c0a12
advaithm <advaith.madhukar@gmail.com>
2020-09-21 18:02:24 +0530

    refactoring to reduce complexit

--- cloudflare_ddns/cli.py
+++ cloudflare_ddns/cli.py
@@ -1,38 +1,39 @@
 from cloudflare_ddns.cloudflareddns import is_connected, read_data_record, verify, read_data, get_record_id, write_data, ddns
 from cloudflare_ddns.gen_settings import gen_settings
 from sys import argv
+
+def getrecords():
+    settings = read_data()
+    data = get_record_id(settings)
+    write_data(data)
+    ddns()
+
 def main():
-    # check if argv has a argument
-    if len(argv)== 1:
-        connected = False
-        while connected is False:
-            connected = is_connected()
-        settings = read_data()
-        data = get_record_id(settings)
-        write_data(data)
-        ddns()
-    # check if that argument is --ddns
-    if len(argv) == 2:
-        if argv[1] == "--ddns":
+    connected = False
+    while connected is False:
+        connected = is_connected()
+    num_experssions = len(argv)
+    if num_experssions == 1:
+        getrecords()
+    elif num_experssions == 2:
+        argument = argv[1]
+        if argument == "--ddns":
             # check if internet is connected
             connected = False
-            while connected == False:
-                connected = is_connected()
-            ddns()
-        elif argv[1] == "--gen-settings":
+        elif  argument == "--gen-settings":
             gen_settings()
-        elif argv[1] == "--verify":
+        elif argument == "--verify":
             settings = read_data_record()
             failed = verify(settings)
             if failed == True:
                 print("settings has failed the intgerty check")
             else:
                 print("settings has passed the intgerty check")
-    # check for usage info handel
-    elif argv[1] == "-h":
-        print(
-            "usage update.py <args:optional>\n-h for this message\n--gen-settings to create settings.yml\n--ddns skip directly to DDNS updateing"
-        )
+        # check for usage info handel
+        elif argument == "-h":
+            print(
+                "usage update.py <args:optional>\n-h for this message\n--gen-settings to create settings.yml\n--ddns skip directly to DDNS updateing"
+            )
     else:
         # error out
         print("Too many args. ")
