f08e0c86e1f8467eac2ed8296c4914d946c2ddc6
advaithm <advaith.madhukar@gmail.com>
2020-09-21 18:06:56 +0530

    refactoring to reduce complexity

--- cloudflare_ddns/cli.py
+++ cloudflare_ddns/cli.py
@@ -1,12 +1,22 @@
 from cloudflare_ddns.cloudflareddns import is_connected, read_data_record, verify, read_data, get_record_id, write_data, ddns
 from cloudflare_ddns.gen_settings import gen_settings
-from sys import argv
+from sys import argv, exit
 
 def getrecords():
     settings = read_data()
     data = get_record_id(settings)
     write_data(data)
     ddns()
+
+def verify_record():
+    settings = read_data_record()
+    failed = verify(settings)
+    if failed == True:
+        print("settings has failed the intgerty check")
+        exit(0)
+    print("settings has passed the intgerty check")
+        # check for usage info handel
+       
 
 def main():
     connected = False
@@ -18,22 +28,15 @@ def main():
     elif num_experssions == 2:
         argument = argv[1]
         if argument == "--ddns":
-            # check if internet is connected
-            connected = False
+            ddns()
         elif  argument == "--gen-settings":
             gen_settings()
         elif argument == "--verify":
-            settings = read_data_record()
-            failed = verify(settings)
-            if failed == True:
-                print("settings has failed the intgerty check")
-            else:
-                print("settings has passed the intgerty check")
-        # check for usage info handel
+            verify_record()
         elif argument == "-h":
             print(
                 "usage update.py <args:optional>\n-h for this message\n--gen-settings to create settings.yml\n--ddns skip directly to DDNS updateing"
-            )
+            )  
     else:
         # error out
         print("Too many args. ")
