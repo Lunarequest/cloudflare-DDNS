a7172cfa3975d475461924ce7280e03ab003e5c8
advaithm <advaith.madhukar@gmail.com>
2020-09-21 18:34:23 +0530

    reduced complexity of verify.py

--- cloudflare_ddns/verify.py
+++ cloudflare_ddns/verify.py
@@ -1,37 +1,36 @@
 import requests
 
+def make_requests(zone_id,record_id,api_key):
+    headers = {"content-type": "application/json", "Authorization": f"Bearer {api_key}"}
+    response = requests.get(
+            f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
+            headers=headers,)
+    return response
+
+def request_status(response,requested):
+    if response.status_code == 200:
+        requested.append(response)
+    else:
+        global failed;failed=True
+    return requested
 def verify(settings):
     """
     function to verify all settings are valid.
 
     :param settings: (dict) needs to be in the form of the dict returned by read data
     """
-    failed = False
+    global failed;failed = False
     requested = []
     zone_id = settings["zone_id"]
     api_key = settings["api_key"]
     record_id = settings["record_id"]
-    headers = {"content-type": "application/json", "Authorization": f"Bearer {api_key}"}
-    response = requests.get(
-            f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
-            headers=headers,)
-    if response.status_code == 200:
-        requested.append(response)
-    else:
-        failed=True
-        return failed
+    response=make_requests(zone_id,record_id,api_key)
+    requested=request_status(response,requested)
     if settings["subdoamins"] != None:
         for record_id in settings["subdomains_id"]:
             # gets the record via api
-            response = requests.get(
-            f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
-            headers=headers,)       
-            if response.status_code==200:
-                for old_request in requested:
-                    if response.json()==old_request.json():
-                            failed= True
-            else:
-                failed = True
+            response=make_requests(zone_id,record_id,api_key)
+            requested=request_status(response,requested)
     return failed
 
     
