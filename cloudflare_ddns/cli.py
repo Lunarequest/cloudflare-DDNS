dd7bae22bf028294c30e0c180996eda1ce88750a
advaithm <advaith.madhukar@gmail.com>
2020-09-21 17:48:43 +0530

    refactoring to reduce complexit

--- cloudflare_ddns/cli.py
+++ cloudflare_ddns/cli.py
@@ -2,32 +2,32 @@ from cloudflare_ddns.cloudflareddns import is_connected, read_data_record, verif
 from cloudflare_ddns.gen_settings import gen_settings
 from sys import argv
 def main():
-    """called by cli. probably can't be used anywhere"""
     # check if argv has a argument
-    if len(argv) < 2:
+    if len(argv)== 1:
         connected = False
-        while connected == False:
+        while connected is False:
             connected = is_connected()
         settings = read_data()
         data = get_record_id(settings)
         write_data(data)
         ddns()
     # check if that argument is --ddns
-    elif argv[1] == "--ddns":
-        # check if internet is connected
-        connected = False
-        while connected == False:
-            connected = is_connected()
-        ddns()
-    elif argv[1] == "--gen-settings":
-        gen_settings()
-    elif argv[1] == "--verify":
-        settings = read_data_record()
-        failed = verify(settings)
-        if failed == True:
-            print("settings has failed the intgerty check")
-        else:
-            print("settings has passed the intgerty check")
+    if len(argv) == 2:
+        if argv[1] == "--ddns":
+            # check if internet is connected
+            connected = False
+            while connected == False:
+                connected = is_connected()
+            ddns()
+        elif argv[1] == "--gen-settings":
+            gen_settings()
+        elif argv[1] == "--verify":
+            settings = read_data_record()
+            failed = verify(settings)
+            if failed == True:
+                print("settings has failed the intgerty check")
+            else:
+                print("settings has passed the intgerty check")
     # check for usage info handel
     elif argv[1] == "-h":
         print(
@@ -36,3 +36,5 @@ def main():
     else:
         # error out
         print("Too many args. ")
+if __name__ == "__main__":
+    main()
